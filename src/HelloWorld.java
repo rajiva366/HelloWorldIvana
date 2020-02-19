d5477fc1d82581ff10b319b5a215dc1504ff473c
rajiva366 <raj_iva@yahoo.com>
2020-02-19 21:18:19 +0100

    My commite

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.print("Ja se zovem Ivana, a tata me zove zlato!");
+		System.out.println("  provera pisanja teksta u nastavku bez println");
+		
+		
+	}
+
+
+}
